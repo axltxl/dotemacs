0621963f2ce60483f58c5ca16df197079ca1717e
Alejandro Ricoveri <alejandroricoveri@gmail.com>
2019-06-12 16:19:24 +0200

    Apply license to init.el file

--- init.el
+++ init.el
@@ -1,10 +1,22 @@
-;; My minimalist emacs config
-;; My attempt to take what I consider the best from spacemacs
-;; and replicate it onto a way slimmed-down minimalist config that
-;; is especially made for my needs
-;; (this is still a work in progress)
-;; STATE: experimental
-;; -----------------------------
+;; Copyright (c) 2019 Alejandro Ricoveri
+
+;; Permission is hereby granted, free of charge, to any person obtaining a copy
+;; of this software and associated documentation files (the "Software"), to deal
+;; in the Software without restriction, including without limitation the rights
+;; to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+;; copies of the Software, and to permit persons to whom the Software is
+;; furnished to do so, subject to the following conditions:
+
+;; The above copyright notice and this permission notice shall be included in
+;; all copies or substantial portions of the Software.
+
+;; THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+;; IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+;; FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+;; AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+;; LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+;; OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+;; THE SOFTWARE.
 
 ;; Setup MELPA as the package repo
 (require 'package)
